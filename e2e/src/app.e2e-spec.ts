593fdc764e2ce263d6a8d18fcb881a9f7c1dee2e
tsabunkar <tsabunkar@gmail.com>
2018-12-14 21:56:37 +0530

    e2e testing :  heroes interaction

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,7 +2,7 @@ import { AppPage } from './app.po';
 import { browser, by, element } from 'protractor';
 
 // !Here we do e2e testing by executing it() inside describe()
-describe('workspace-project App', () => {
+describe('e2e testing App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -20,6 +20,12 @@ describe('workspace-project App', () => {
     expect(page.getParagraphText()).toEqual('Tour of Heroes');
   });
 
+  // !Altrenatively
+  it('should have test as Tour of Heroes in h1 tag (Altrenative)', () => {
+    browser.get('http://localhost:4200/dashboard');
+    expect(page.getParagraphText()).toEqual('Tour of Heroes');
+  });
+
   it('should display Dashboard button and Heores Button inside nav tag', () => {
     page.navigateTo();
     expect(page.getDashboardButton().getText()).toEqual('Dashboard');
@@ -34,7 +40,36 @@ describe('workspace-project App', () => {
     expect(page.getHeroesPageText()).toEqual('My Heroes');
   });
 
+  it('should enter value inside the input text box', () => {
+    page.navigateToHeroes();
 
+    page.sendValueInsideInputElement();
+
+    browser.sleep(3000);
+
+    expect(page.getValueInsideInputElement()).toEqual('Batman');
+  });
+
+  it('should add new hero in list when add btn is click on entering text value in input', () => {
+    page.navigateToHeroes();
+
+    page.sendValueInsideInputElement(); // value is entered inside input text box
+
+    browser.sleep(2000);
+
+    page.getAddButton().click(); // add button is clicked
+
+    browser.sleep(2000);
+
+    page.findLatestAddedElementInTheListOfHeroes() // check latest added li element text content matched with Batman
+      .then((liText_Array) => {
+        const idAndNameElements = liText_Array.split('\n');
+        const nameElements = idAndNameElements[0].split(' ');
+        const requiredNameInLiElement = nameElements[nameElements.length - 1];
+        expect(requiredNameInLiElement).toEqual('Batman');
+      });
+
+  });
 
 
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,8 +1,10 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, $$ } from 'protractor';
 
 // ! This file is called - page object (app.po.ts)
 // !In this page object file we write all the functions which we expecting our application to have/behave
 export class AppPage {
+
+  // !note: all this function must return promise or element
   navigateTo() {
     return browser.get('/');
   }
@@ -11,10 +13,6 @@ export class AppPage {
     return element(by.css('app-root h1')).getText();
   }
 
-  /* <nav>
-  <a routerLink="/dashboard">Dashboard</a>
-  <a routerLink="/heroes">Heroes</a>
-</nav> */
   getDashboardButton() {
     return element(by.css('[routerlink="/dashboard"]'));
   }
@@ -25,6 +23,37 @@ export class AppPage {
 
   getHeroesPageText() {
     return element(by.css('app-heroes h2')).getText();
+  }
+
+  navigateToHeroes() {
+    return browser.get('/heroes');
+  }
+
+  // !Enterting value inside the input text box
+  sendValueInsideInputElement() {
+    // locating element using xpath ->
+    return element(by.xpath('/html/body/app-root/app-heroes/div/label/input')).sendKeys('Batman');
+
+    // to get xpath of an element > f12 > Inspect Elemet > select element >
+    // right click that element (in Element window) -> Copy -> Copy XPath
+
+  }
+
+  // !checking the entered input text value inside the text box is correct
+  getValueInsideInputElement() {
+    // locating element using css ->
+    return element(by.css('app-heroes div label input')).getAttribute('value');
+  }
+
+  getAddButton() {
+    return element(by.xpath('/html/body/app-root/app-heroes/div/button'));
+  }
+
+  findLatestAddedElementInTheListOfHeroes() {
+
+    const lastLiElement = element.all(by.xpath('/html/body/app-root/app-heroes/ul/li')).last();
+    return lastLiElement.getText();
+
   }
 
 }
